4b060d5ec3d4e7d612e5bb357454db5092d51b46
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-01 16:52:17 -0600

    added hidden status to Contact Me form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -9,12 +9,12 @@ const ContactMe = () =>{
             <a href="https://www.linkedin.com/in/joshuapeavler/">LinkedIn</a>
             <a href="https://github.com/jpeavler">Github</a>
             <p>Email: joshuaapeavler@gmail.com</p>
-            {/* <form>
+            <form hidden>
                 <input type="text" placeholder="Name"/>
                 <input type="email" placeholder="Email"/>
                 <textarea type="text" placeholder="Message"/>
                 <input type="submit" value="Comments"/>
-            </form> */}
+            </form>
         </>
     )
 }
