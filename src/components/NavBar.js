0691ddbaadfa266254510ae4f1392aad906d1b2c
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-01 16:52:46 -0600

    Minor styling to Navagation Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {
+    Navbar
+} from 'reactstrap';
 
 const NavBar = () =>{
     return(
-        <nav>
+        <Navbar color="light">
             <Link to='/' className="NavLink">Home</Link>
             <Link to='/Resume' className="NavLink">Resume</Link>
             <Link to='/Projects' className="NavLink">Projects</Link>
             <Link to='/Contact' className="NavLink">Contact Me</Link>
-        </nav>
+        </Navbar>
     )
 }
 
