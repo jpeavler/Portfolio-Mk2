a52de3deba9f3063bed5fe43516d1d8cdef462d4
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-01 17:08:00 -0600

    implemented cards into project page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar';
+import {
+    Card, 
+    CardImg, 
+    CardText, 
+    CardBody,
+    CardTitle,
+    CardLink
+} from 'reactstrap'
 
 const Projects = () =>{
     return(
@@ -7,25 +15,18 @@ const Projects = () =>{
             <NavBar/>
             <h2>Projects</h2>
             <div className="projects">
-                <div className='project'>
-                    <h4>Game Collection</h4>
-                    <a href="https://game-collection-jpeavler2.herokuapp.com/">Deployed Project Link</a>
-                    <a href="https://github.com/jpeavler/video-games-api">Github Link</a>
-                    <img src="/assets/gameCollectionProject.png" alt="Game Collection Project" width="500px"/> {/*TODO: Transfer width to CSS */}
-                    <p>
+            <Card>
+                <CardImg top src="/assets/gameCollectionProject.png" alt="Game Collection Project"/>
+                <CardBody>
+                    <CardTitle>Game Collection</CardTitle>
+                    <CardText>
                     A website that allows users to view, create, edit, or delete descriptions of games in a game collection. The Video Games tab is
                     the only functional page as I plan to add additional game collections in the future.
-                    </p>
-                </div>
-                <div className='project'>
-                    <h4>Project 2</h4>
-                </div>
-                <div className='project'>
-                    <h4>Project 3</h4>
-                </div>
-                <div className='project'>
-                    <h4>Project 4</h4>
-                </div>
+                    </CardText>
+                    <CardLink href="https://game-collection-jpeavler2.herokuapp.com/">Deployed Project Link</CardLink>
+                    <CardLink href="https://github.com/jpeavler/video-games-api">Github Link</CardLink>
+                </CardBody>
+            </Card>
             </div>
         </>
     )
