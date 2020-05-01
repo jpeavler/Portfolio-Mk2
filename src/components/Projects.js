0452f4bd3e1bd98a2d9f93d079a38072a31ac7af
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-01 11:51:52 -0600

    added content to Projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,28 @@ const Projects = () =>{
     return(
         <>
             <NavBar/>
-            <h2>This is the Projects Page</h2>
+            <h2>Projects</h2>
+            <div className="projects">
+                <div className='project'>
+                    <h4>Game Collection</h4>
+                    <a href="https://game-collection-jpeavler2.herokuapp.com/">Deployed Project Link</a>
+                    <a href="https://github.com/jpeavler/video-games-api">Github Link</a>
+                    <img src="/assets/gameCollectionProject.png" alt="Game Collection Project" width="500px"/> {/*TODO: Transfer width to CSS */}
+                    <p>
+                    A website that allows users to view, create, edit, or delete descriptions of games in a game collection. The Video Games tab is
+                    the only functional page as I plan to add additional game collections in the future.
+                    </p>
+                </div>
+                <div className='project'>
+                    <h4>Project 2</h4>
+                </div>
+                <div className='project'>
+                    <h4>Project 3</h4>
+                </div>
+                <div className='project'>
+                    <h4>Project 4</h4>
+                </div>
+            </div>
         </>
     )
 }
