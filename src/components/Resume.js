03aa72c34d1e04e54059cdde027497ba08e24212
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-01 12:02:01 -0600

    adjusted header of resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ const Resume = () =>{
     return(
         <>
             <NavBar/>
-            <h2>This is the Resume Page</h2>
+            <h2>Resume</h2>
             <iframe title="resume" src="/assets/Resume.pdf" width="80%" height="900px"></iframe>
         </>
     )
