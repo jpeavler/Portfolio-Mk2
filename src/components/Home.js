cff18bd71f5ea78c6e072c5f31ef3bfe28218505
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-08 11:47:23 -0600

    Stylized Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import NavBar from './NavBar';
+import {
+    Card, 
+    CardImg, 
+    CardText, 
+    CardBody,
+    CardLink,
+    Row,
+    Col
+} from 'reactstrap';
 
 const Home = () =>{
     return(
         <>
             <NavBar/>
             <h2>Home</h2>
-            <p>Aspiring web developer. Learning full stack development at Helio Training.</p>
+            <Row>
+                <Col sm="2"></Col>
+                <Col sm="8">
+                    <Card>
+                        <CardBody>
+                            <CardText>
+                            Aspiring web developer. Learning full stack development at Helio Training.
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
