68d6e0b7a4b7501fe5b2555396f38aea17d379bf
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-08 11:30:11 -0600

    Stylized Contact Me page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import NavBar from './NavBar';
+import {
+    Card, 
+    CardImg, 
+    CardText, 
+    CardBody,
+    CardLink,
+    Row,
+    Col
+} from 'reactstrap'
 
 const ContactMe = () =>{
     return(
         <>
             <NavBar/>
             <h2>Contact Me</h2>
-            <a href="https://www.linkedin.com/in/joshuapeavler/">LinkedIn</a>
-            <a href="https://github.com/jpeavler">Github</a>
-            <p>Email: joshuaapeavler@gmail.com</p>
-            <form hidden>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder="Email"/>
-                <textarea type="text" placeholder="Message"/>
-                <input type="submit" value="Comments"/>
-            </form>
+            <Row>
+                <Col sm="1"></Col>
+                <Col sm="2">
+                    <Card>
+                        <CardImg src="/assets/ProfilePicture.jpg" alt="Joshua Peavler"/>
+                    </Card>
+                </Col>
+                <Col sm="8">
+                    <Card style={{ height: '18rem'}}>
+                        <CardBody>
+                            <CardText style={{ "text-align": "Left"}}>
+                                If you would like to reach out to talk about coding projects, feel free to
+                                reach me via Github. If you are interested in networking, feel free to 
+                                contact me via LinkedIn or Github. I'm also always open via email as well.
+                            </CardText>
+                        <CardLink href="https://www.linkedin.com/in/joshuapeavler/">LinkedIn</CardLink>
+                        <CardLink href="https://github.com/jpeavler">Github</CardLink>
+                        <CardText>Email: joshuaapeavler@gmail.com</CardText>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
