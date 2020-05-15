13e5531e7f23143841b73e1dcd9b6831bc92c089
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-15 11:11:36 -0600

    Replaced Home in NavBar with Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,9 @@ import {Link} from 'react-router-dom';
 const NavBar = () =>{
     return(
         <nav>
-            <Link to='/' className="NavLink">Home</Link>
+            <Link to='/' className="NavLink">
+                <img src="/favicon.ico" height="30px"/>
+            </Link>
             <Link to='/Resume' className="NavLink">Resume</Link>
             <Link to='/Projects' className="NavLink">Projects</Link>
             <Link to='/Contact' className="NavLink">Contact Me</Link>
