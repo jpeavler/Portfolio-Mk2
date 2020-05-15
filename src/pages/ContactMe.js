251f85b3adaf5a1e8ba97bb2b68d139609078f9e
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-15 10:04:55 -0600

    Implemented react-strap CardDeck component, replaced Col and Row components

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import {
+    CardDeck,
     Card, 
     CardImg, 
     CardText, 
     CardBody,
     CardLink,
-    Row,
-    Col
 } from 'reactstrap'
 
 const ContactMe = () =>{
@@ -15,28 +14,23 @@ const ContactMe = () =>{
         <>
             <NavBar/>
             <h2>Contact Me</h2>
-            <Row>
-                <Col sm="1"></Col>
-                <Col sm="2">
-                    <Card body outline color="secondary" className="card">
-                        <CardImg src="/assets/ProfilePicture.jpg" alt="Joshua Peavler"/>
-                    </Card>
-                </Col>
-                <Col sm="8">
-                    <Card style={{ height: '18rem'}} body outline color="secondary" className="card">
-                        <CardBody>
-                            <CardText style={{ "text-align": "Left"}}>
-                                If you would like to reach out to talk about coding projects, feel free to
-                                reach me via Github. If you are interested in networking, feel free to 
-                                contact me via LinkedIn or Github. I'm also always open via email as well.
-                            </CardText>
-                        <CardLink target="_blank" href="https://www.linkedin.com/in/joshuapeavler/">LinkedIn</CardLink>
-                        <CardLink target="_blank" href="https://github.com/jpeavler">Github</CardLink>
-                        <CardText>Email: joshuaapeavler@gmail.com</CardText>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
+            <CardDeck>
+                <Card body outline color="secondary" className="profile-card">
+                    <CardImg className="profile" src="/assets/ProfilePicture.jpg" alt="Joshua Peavler"/>
+                </Card>
+                <Card body outline color="secondary" className="card">
+                    <CardBody>
+                        <CardText style={{ "textAlign": "Left"}}>
+                            If you would like to reach out to talk about coding projects, feel free to
+                            reach me via Github. If you are interested in networking, feel free to 
+                            contact me via LinkedIn or Github. I'm also always open via email as well.
+                        </CardText>
+                    <CardLink target="_blank" href="https://www.linkedin.com/in/joshuapeavler/">LinkedIn</CardLink>
+                    <CardLink target="_blank" href="https://github.com/jpeavler">Github</CardLink>
+                    <CardText>Email: joshuaapeavler@gmail.com</CardText>
+                    </CardBody>
+                </Card>
+            </CardDeck>
         </>
     )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import {
+    CardDeck,
     Card, 
     CardImg, 
     CardText, 
     CardBody,
     CardTitle,
     CardLink,
-    Row,
-    Col
 } from 'reactstrap'
 
 const Projects = () =>{
@@ -16,42 +15,35 @@ const Projects = () =>{
         <>
             <NavBar/>
             <h2>Projects</h2>
-            <div className="projects">
-            <Row>
-                <Col sm = "1"></Col>
-                <Col sm ="5">
-                    <Card body outline color="secondary" className="card">
-                        <CardImg top src="/assets/gameCollectionProject.png" alt="Game Collection Project"/>
-                        <CardBody>
-                            <CardTitle>Game Collection</CardTitle>
-                            <CardText>
-                            I personally liked how I styled this project, it was my first time I felt that I made a nice looking website.
-                            A website that allows users to view, create, edit, or delete descriptions of games in a game collection. The Video Games tab is
-                            the only functional page as I plan to add additional game collections in the future. Utilizes a MERN fullstack.
-                            </CardText>
-                            <CardLink target="_blank" href="https://game-collection-jpeavler2.herokuapp.com/">Deployed Project Link</CardLink>
-                            <CardLink target="_blank" href="https://github.com/jpeavler/video-games-api">Github Link</CardLink>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col sm ="5">
-                    <Card body outline color="secondary" className="card">
-                        <CardImg top src="/assets/WebDevTerms.png" alt="Web Development Terminology Project"/>
-                        <CardBody >
-                            <CardTitle>Web Development Terminology</CardTitle>
-                            <CardText>
-                            I share this project since this is the most robust project I've made functionally speaking.
-                            A Computer Science terminology wiki that allows the user to store terms with their own resources.
-                            Once a term is added to the wiki, additional resources can be inserted or removed. Allows user to archive and
-                            delete archived terms. The two buttons at top allow the user to hide archived and active terms from view.
-                            </CardText>
-                            <CardLink target="_blank" href="https://learners-catalog.herokuapp.com/">Deployed Project Link</CardLink>
-                            <CardLink target="_blank" href="https://github.com/jpeavler/Learners-Catalog">Github Link</CardLink>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-            </div>
+            <CardDeck>
+                <Card body outline color="secondary" className="card">
+                    <CardImg top src="/assets/gameCollectionProject.png" alt="Game Collection Project"/>
+                    <CardBody>
+                        <CardTitle>Game Collection</CardTitle>
+                        <CardText>
+                        I personally liked how I styled this project, it was my first time I felt that I made a nice looking website.
+                        A website that allows users to view, create, edit, or delete descriptions of games in a game collection. The Video Games tab is
+                        the only functional page as I plan to add additional game collections in the future. Utilizes a MERN fullstack.
+                        </CardText>
+                        <CardLink target="_blank" href="https://game-collection-jpeavler2.herokuapp.com/">Deployed Project Link</CardLink>
+                        <CardLink target="_blank" href="https://github.com/jpeavler/video-games-api">Github Link</CardLink>
+                    </CardBody>
+                </Card>
+                <Card body outline color="secondary" className="card">
+                    <CardImg top src="/assets/WebDevTerms.png" alt="Web Development Terminology Project"/>
+                    <CardBody >
+                        <CardTitle>Web Development Terminology</CardTitle>
+                        <CardText>
+                        I share this project since this is the most robust project I've made functionally speaking.
+                        A Computer Science terminology wiki that allows the user to store terms with their own resources.
+                        Once a term is added to the wiki, additional resources can be inserted or removed. Allows user to archive and
+                        delete archived terms. The two buttons at top allow the user to hide archived and active terms from view.
+                        </CardText>
+                        <CardLink target="_blank" href="https://learners-catalog.herokuapp.com/">Deployed Project Link</CardLink>
+                        <CardLink target="_blank" href="https://github.com/jpeavler/Learners-Catalog">Github Link</CardLink>
+                    </CardBody>
+                </Card>
+            </CardDeck>
         </>
     )
 }
