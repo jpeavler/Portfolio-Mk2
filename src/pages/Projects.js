550504fe88dc77dbb1220e7900e3a6cea061925c
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-29 16:57:48 -0600

    Added Tic Tac Toe React Project to Project Page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import data from '../project_data/project.json'
+import Game from '../project_data/TicTacToe/TicTacToe'
+import '../project_data/TicTacToe/TicTacToe.css'
 import {
     CardDeck,
     Card, 
@@ -31,6 +33,18 @@ const Projects = () =>{
             <h2>Projects</h2>
             <h3>Full Stack Projects</h3>
             <CardDeck className="full">{displayFSProjects}</CardDeck>
+            <h3>Small Projects</h3>
+
+            <CardDeck>
+                <Card outline color="secondary" className="card">
+                    <CardBody>
+                        <Game/>
+                        <CardTitle>React Tic Tac Toe</CardTitle>
+                        <CardText>My first project using React, I followed the React tutorial to make this Tic Tac Toe game.</CardText>
+                        <CardLink target="_blank" href="">Github Link</CardLink>
+                    </CardBody>
+                </Card>
+            </CardDeck>
         </>
     )
 }
