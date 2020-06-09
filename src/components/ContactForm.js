f8b3d03ebde55c257be3d49aff799c452020277b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-09 17:00:36 -0600

    Initialize Contact Me form

---
+++ src/components/ContactForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import {Form} from 'reactstrap';
+
+const ContactForm = () => {
+    return (
+        <Form>
+            
+        </Form>
+    )
+}
+
+export default ContactForm;
